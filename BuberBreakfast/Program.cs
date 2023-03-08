467d3df34af78f120cd1eaabd2cbabcface2cace
Faseun <faseunoluwatobilobasimon@gmail.com>
2023-03-08 21:35:57 +0100

    more comment explaining the program and aslso changed the builder.service function from add scope to add singleton need to read more on that

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -6,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 {
     builder.Services.AddControllers();
-    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+    builder.Services.AddSingleton<IBreakfastService, BreakfastService>();
+    // the above line defines how the breakfast controller object is created and what is passed to it 
+    // because without it the application wont know what to pass as parameter to the newly created 
+    // breakfast controller object
+    // always check out the difference between addScoped, addSingleton and AddTransient
 }
 
 var app = builder.Build();
--- BuberBreakfast/Services/Breakfasts/BreakfastService.cs
+++ BuberBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -4,15 +4,23 @@ using BuberBreakfast.Models;
 namespace BuberBreakfast.Services.Breakfasts
 {
     public class BreakfastService : IBreakfastService
+    // this class is going to inherit from the IBreakfast service withe interface class
+    // and you can now overwrite and create your functions in it
 
     {
         private static readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+        // this dictionary is To Only pair id and name 
         public void CreateBreakfast(Breakfast breakfast)
+        // this method overrides the interface class create Breakfast method
         {
             _breakfasts.Add(breakfast.Id, breakfast);
+            // add the breakfast object and id to the dictionary
         }
         public Breakfast GetBreakfast(Guid id)
         {
+            // this method will take an id as a parameter and look through the dictionary
+            // and return the object with that id
+            // NOTE: we are overriding the method in the IBreakfastService interface class
             return _breakfasts[id];
         }
     }
